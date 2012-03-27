b69e7ce855fbcfd84034400cc8d92884e6e6300a
linkcd <linkcd@gmail.com>
2012-03-27 20:20:27 +0200

    generate ror

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyERP::Application.config.secret_token = 'ee3bef82c4a6334bc48703ee3359b70d0c238f1a4ba49266b7f4aea3bfcbddeefa43f1ce08b80b0f2c2f5ccb6601c0ee51eade71089720aaf9748e2927f8ea17'
