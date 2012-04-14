719babae5fbbed3a07968482843db25af6211b7d
linkcd <linkcd@gmail.com>
2012-04-14 17:41:05 +0800

    update inventory display page

---
+++ db/migrate/20120414093553_add_name_to_item.rb
@@ -0,0 +1,6 @@
+class AddNameToItem < ActiveRecord::Migration
+  def change
+    add_column :items, :name, :string
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120413075829) do
+ActiveRecord::Schema.define(:version => 20120414093553) do
 
   create_table "inventories", :force => true do |t|
     t.integer  "item_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20120413075829) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "name"
   end
 
   create_table "location_properties", :force => true do |t|
